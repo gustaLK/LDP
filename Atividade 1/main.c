d179876d7a6dfd20b57c4791d8c66d6ae0b8a57a
gustaLK <ghamaral278@gmail.com>
2026-08-18 22:02:53 -0300

    Add files via upload

---
+++ Atividade 1/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+extern float Media(float Matriz[12][12], int Linha);
+extern float Soma(float Matriz[12][12], int Linha);
+int main()
+{
+    float Matr[12][12], Result;
+    int Linha;
+    char Oper;
+
+    scanf("%d", &Linha);
+    scanf(" %c", &Oper);
+    for(int l=0; l<2; l++){
+        for(int c=0; c<2; c++){
+            scanf("%f", &Matr[l][c]);
+        }
+    }
+    if(Oper=='S'){
+        Result= Soma(Matr, Linha);
+    }
+    else if(Oper=='M'){
+        Result= Media(Matr, Linha);
+    }
+    printf("%.1f", Result);
+
+    return 0;
+}
