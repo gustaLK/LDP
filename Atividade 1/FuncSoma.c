40ba1dc790522d1b354f12b9e8df823513c4849e
gustaLK <ghamaral278@gmail.com>
2026-08-19 11:57:49 -0300

    Update FuncSoma.c

--- Atividade 1/FuncSoma.c
+++ Atividade 1/FuncSoma.c
@@ -1,8 +1,9 @@
 float Soma(float Matriz[12][12], int Linha)
 {
     float Result=0;
-    for(int i=0; i<12; i++){
+    for(int i=0; i<12; i++)
+    {
         Result += Matriz[Linha][i];
     }
-    return(Result);
+    return (Result);
 }
