81598eb886a6fd6b00e776ba3d68c1e3dd5b4a50
gustaLK <ghamaral278@gmail.com>
2026-08-19 10:40:52 -0300

    Update FuncMedia.c

--- Atividade 1/FuncMedia.c
+++ Atividade 1/FuncMedia.c
@@ -1,7 +1,7 @@
 float Media(float Matriz[12][12], int Linha)
 {
     float Result;
-    for(int i=0; i<2; i++){
+    for(int i=0; i<12; i++){
         Result= Result+Matriz[Linha][i];
     }
     Result= Result/2;
