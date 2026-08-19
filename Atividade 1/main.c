087406c8ba88bfd1c85aaf12c0881d250e30ece9
gustaLK <ghamaral278@gmail.com>
2026-08-19 11:58:21 -0300

    Update main.c

--- Atividade 1/main.c
+++ Atividade 1/main.c
@@ -10,8 +10,10 @@ int main()
 
     scanf("%d", &Linha);
     scanf(" %c", &Oper);
-    for(int l=0; l<12; l++){
-        for(int c=0; c<12; c++){
+    for(int l=0; l<12; l++)
+    {
+        for(int c=0; c<12; c++)
+        {
             scanf("%f", &Matr[l][c]);
         }
     }
