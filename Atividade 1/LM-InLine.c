47c9612540158ad7fdb4f18f16ffb1e5ef50c670
gustaLK <ghamaral278@gmail.com>
2026-08-19 10:45:19 -0300

    Create LM-InLine.c

---
+++ Atividade 1/LM-InLine.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+float Soma(float Matriz[12][12], int Linha)
+{
+    float Result = 0;
+    for (int i = 0; i < 12; i++)
+    {
+        Result = Result + Matriz[Linha][i];
+    }
+    return (Result);
+}
+
+float Media(float Matriz[12][12], int Linha)
+{
+    float Result;
+    for (int i = 0; i < 12; i++)
+    {
+        Result = Result + Matriz[Linha][i];
+    }
+    Result = Result / 2;
+    return (Result);
+}
+
+int main()
+{
+    float Matr[12][12], Result;
+    int Linha;
+    char Oper;
+
+    scanf("%d", &Linha);
+    scanf(" %c", &Oper);
+    for (int l = 0; l < 12; l++)
+    {
+        for (int c = 0; c < 12; c++)
+        {
+            scanf("%f", &Matr[l][c]);
+        }
+    }
+    if (Oper == 'S')
+    {
+        Result = Soma(Matr, Linha);
+    }
+    else if (Oper == 'M')
+    {
+        Result = Media(Matr, Linha);
+    }
+    printf("%.1f", Result);
+
+    return 0;
+}
