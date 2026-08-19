20a19b710afcb1e0e9a5034af0e493c227944ea4
gustaLK <ghamaral278@gmail.com>
2026-08-18 21:48:24 -0300

    Update FuncSoma;.c

--- Aticidade 1/LinhaNaMatriz/FuncSoma;.c
+++ Aticidade 1/LinhaNaMatriz/FuncSoma;.c
@@ -2,7 +2,7 @@ float Soma(float Matriz[12][12], int Linha)
 {
     float Result=0;
     for(int i=0; i<2; i++){
-        Result= Result + Matriz[Linha, i];
+        Result= Result + Matriz[Linha][i];
     }
     return(Result);
 }
