fadf003dc4a8a730d1fcbd4af654bfbc39ef8d9d
gustaLK <ghamaral278@gmail.com>
2026-09-04 21:44:27 -0300

    Create Ativ1Exe2.c

---
+++ Exemplos C/Ativ1Exe2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+void SubString(char origem[], char destino[])
+{
+    int i;
+
+    while(origem[i] != ' '){
+        destino[i]=origem[i];
+        i++;
+    }
+    destino[i]='\0';
+}
+
+void SubString1(char origem[], char destino[]){
+    int i=0, j=strlen(origem);
+
+    while(origem[j] != ' ')
+        j--;
+
+    j++;
+    while(origem[j] != '\0'){
+        destino[i]=origem[j];
+        j++;
+        i++;
+    }
+    destino[i]='\0';
+}
+
+int main(){
+    char Nome[101], PrimeiroNome[36], UltimoNome[36];
+
+    scanf("%[^\n]", Nome);
+
+    SubString1(Nome, UltimoNome);
+    printf("%s,", UltimoNome);
+
+    SubString(Nome, PrimeiroNome);
+    printf("%s\n", PrimeiroNome);
+
+    return 0;
+}
