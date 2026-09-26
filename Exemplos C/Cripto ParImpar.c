5170c9b6960f24701c5c1248cf6fc4edd0436f2f
gustaLK <ghamaral278@gmail.com>
2026-09-25 21:56:20 -0300

    Add files via upload

---
+++ Exemplos C/Cripto ParImpar.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+void cripto(char *mensagem, char *msgCripto)
+{
+    int i;
+    for(i=0; i<strlen(mensagem); i++){
+        if(isalpha(*(mensagem+i))){
+            if((1+i)%2==0){
+                *(msgCripto+i)= *(mensagem+i)-1;
+                if(*(msgCripto+i)=='`' || *(msgCripto+i)=='@')
+                    *(msgCripto+i)= *(msgCripto+i)+26;
+            }
+            else
+                *(msgCripto+i)= *(mensagem+i)+1;
+                if(*(msgCripto+i)=='{' || *(msgCripto+i)=='[')
+                    *(msgCripto+i)= *(msgCripto+i)-26;
+        }
+        else
+            *(msgCripto+i)= *(mensagem+i);
+    }
+    *(msgCripto+i)= '\0';
+}
+
+int main(){
+    char mensagem[41], msgCripto[41];
+
+    printf("Digite uma palavra: ");
+    scanf("%[^\n]", mensagem);
+
+    cripto(mensagem, msgCripto);
+
+    printf("%s", msgCripto);
+
+    return 0;
+}
